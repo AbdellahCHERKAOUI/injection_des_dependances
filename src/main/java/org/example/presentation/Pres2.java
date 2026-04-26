bc8a126c53b1deb126f31b529f6be55654e51f61
Cherkaoui <cherkaouia336@gmail.com>
2026-04-26 11:34:51 +0200

    injection des dependences d'une maniere dynamique

--- src/main/java/org/example/presentation/Pres2.java
+++ src/main/java/org/example/presentation/Pres2.java
@@ -1,2 +1,31 @@
-package org.example.presentation;public class Pres2 {
+package org.example.presentation;
+
+import org.example.dao.IDao;
+import org.example.metier.IMetier;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Pres2 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("src/main/resources/config.txt")
+        );
+
+        // DAO
+        String daoClassName = br.readLine();
+        Class<?> cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.getDeclaredConstructor().newInstance();
+
+        // METIER
+        String metierClassName = br.readLine();
+        Class<?> cMetier = Class.forName(metierClassName);
+
+        IMetier metier = (IMetier) cMetier
+                .getConstructor(IDao.class)
+                .newInstance(dao);
+
+        System.out.println("Résultat = " + metier.calcul());
+    }
 }
